3fdf17c47ef09030e12089582f586e5aa5f63f5a
callme-devil <pransermi@gmail.com>
2022-07-11 18:10:43 +0430

    A) DataAnnotations

--- EntityFramework_Project/Models/Product.cs
+++ EntityFramework_Project/Models/Product.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace EntityFramework_Project.Models
 {
+    //[Table("MyProducts" ,Schema ="PRD(Custom)")]//PRD.MyProducts
     public class Product
     {
-        
+        [Key]
         public int Id { get; set; }
-
+        [Required]
+        [MaxLength(250)]
+        [MinLength(2)]
         public string Name { get; set; }
 
         public double UnitPrice { get; set; }
 
+        //[Column("CreatedDate")]
         public DateTime CreationDate { get; set; }
 
         public bool IsDeleted { get; set; }
